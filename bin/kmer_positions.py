ed91077adadd5acf43cdd4cad1cadac8f5005dfb
Darrin Schultz <darrin.schultz91@gmail.com>
2022-03-13 15:35:36 -0700

    updating the kmer_positions.py script to not include revcomp to allow stranded searches

--- bin/kmer_positions.py
+++ bin/kmer_positions.py
@@ -7,7 +7,7 @@ first positional arg is k size
 second and later positional args are kmers
 
 use like this:
-cat genome.fasta | python telomere_peaks.py 6 TTAGGG CCCTAA | bedtools merge | awk '{if (($3-$2)/6 > 1) {printf("%s\t%d\t%d\t%d\n", $1, $2, $3, ($3-$2)/6 )}}'
+cat genome.fasta | python kmer_positions.py 6 TTAGGG TTAAGGG | bedtools merge | awk '{if (($3-$2)/6 > 1) {printf("%s\t%d\t%d\t%d\n", $1, $2, $3, ($3-$2)/6 )}}'
 
 ^ prints out a bedgraph file (incomplete, no 0 stretches), showing how many kmers are in stretches.
 """
@@ -15,16 +15,8 @@ cat genome.fasta | python telomere_peaks.py 6 TTAGGG CCCTAA | bedtools merge | a
 from Bio import SeqIO
 import sys
 
-def reverse_complement(seq):
-    this_complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-    return "".join(this_complement.get(base, base) for base in reversed(seq))
-
-assembly = "/bigdata/user/darrin/pyrosome/assembly_versions/pyro_hapA_v0.1.fasta"
-
 k = int(sys.argv[1])
-kmers = [x.upper() for x in sys.argv[2::]]
-revcomp = [reverse_complement(x) for x in kmers]
-tkmers = list(set(kmers + revcomp))
+tkmers = list(set([x.upper() for x in sys.argv[2::]]))
 
 for record in SeqIO.parse(sys.stdin, "fasta"):
     positions = []
