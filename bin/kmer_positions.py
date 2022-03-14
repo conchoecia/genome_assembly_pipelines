40ac643acee253d62bf6cca6fc3bbbb6003cbd21
Darrin Schultz <darrin.schultz91@gmail.com>
2022-03-14 16:45:35 -0700

    updating kmer_positions to work correctly

--- bin/kmer_positions.py
+++ bin/kmer_positions.py
@@ -3,11 +3,10 @@
 This program prints out the position of any given kmers. Used with bedtools merge it is 
 useful to get ranges and sizes of telomere or other repeat stretches.
 
-first positional arg is k size
-second and later positional args are kmers
+first and later positional args are kmers. They all must be the same size.
 
 use like this:
-cat genome.fasta | python kmer_positions.py 6 TTAGGG TTAAGGG | bedtools merge | awk '{if (($3-$2)/6 > 1) {printf("%s\t%d\t%d\t%d\n", $1, $2, $3, ($3-$2)/6 )}}'
+cat genome.fasta | python kmer_positions.py TTAGGG TTAAGG | bedtools merge | awk '{if (($3-$2)/6 > 1) {printf("%s\t%d\t%d\t%d\n", $1, $2, $3, ($3-$2)/6 )}}'
 
 ^ prints out a bedgraph file (incomplete, no 0 stretches), showing how many kmers are in stretches.
 """
@@ -15,8 +14,11 @@ cat genome.fasta | python kmer_positions.py 6 TTAGGG TTAAGGG | bedtools merge | 
 from Bio import SeqIO
 import sys
 
-k = int(sys.argv[1])
-tkmers = list(set([x.upper() for x in sys.argv[2::]]))
+tkmers = list(set([x.upper() for x in sys.argv[1::]]))
+kmer_sizes = list(set([len(x) for x in tkmers]))
+if len(kmer_sizes) > 1:
+    raise IOError("All kmers must have the same length. We saw k = {}".format(kmer_sizes))
+k = kmer_sizes[0]
 
 for record in SeqIO.parse(sys.stdin, "fasta"):
     positions = []
